2dbf673a808a07b5b4377fd07b687ecefd9ff58b
Owen Olabode <oladapo2011@gmail.com>
2025-11-12 12:00:41 +0100

    creating order and orderItems in our mutation

--- sick-fits/backend/schemas/ProductImage.ts
+++ sick-fits/backend/schemas/ProductImage.ts
@@ -3,7 +3,7 @@ import { relationship, text } from "@keystone-next/fields";
 import { list } from "@keystone-next/keystone/schema";
 import "dotenv/config";
 
-export const cloudinary = {
+const cloudinary = {
   cloudName: process.env.CLOUDINARY_CLOUD_NAME,
   apiKey: process.env.CLOUDINARY_KEY,
   apiSecret: process.env.CLOUDINARY_SECRET,

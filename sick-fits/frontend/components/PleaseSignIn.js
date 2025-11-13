e2ab0151f60d0ecb2946b9ce2abfd8f348dac445
Owen Olabode <oladapo2011@gmail.com>
2025-11-14 00:04:09 +0100

    Creating a Gated Sign In Component

---
+++ sick-fits/frontend/components/PleaseSignIn.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { useUser } from './User';
+import SignIn from './SignIn';
+
+export default function ({ children }) {
+  const me = useUser();
+  if (!me) return <SignIn />;
+  return children;
+}

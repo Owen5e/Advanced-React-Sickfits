6d0cfb7efb70b438aa1472d41ec3d12aae0185c8
Owen Olabode <oladapo2011@gmail.com>
2025-11-13 01:57:06 +0100

    Permissions Access Functions

--- sick-fits/backend/access.ts
+++ sick-fits/backend/access.ts
@@ -1,7 +1,27 @@
 // AT it's simplest, the access control returns a yes or no value depending on the users session
 
+import { permissionsList } from "./schemas/fields";
 import { ListAccessArgs } from "./types";
 
 export function isSignedIn({ session }: ListAccessArgs) {
   return !!session;
 }
+
+const generatedPermissions = Object.fromEntries(
+  permissionsList.map((permission) => [
+    permission,
+    function ({ session }: ListAccessArgs) {
+      return !!session?.data.role?.[permission];
+    },
+  ])
+);
+
+// Permission checkers - yes or no
+export const permissions = {
+  ...generatedPermissions,
+  isAwesome({ session }: ListAccessArgs): boolean {
+    return session?.data.name.includes("Owen");
+  },
+};
+
+// Rule based access control
